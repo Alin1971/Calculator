16341ada6e2dc851934cb204371dc3e999b14a7a
Biprodeb Roy Alin <biprodebalin@gmail.com>
2023-03-29 13:36:20 +0600

    multiplication added

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,25 @@
 import java.util.Scanner;
 
 public class Calculator {
-        private double sum(double firstValue, double secondValue){
-            Scanner sc = new Scanner(System.in);
-            firstValue = sc.nextDouble();
-            secondValue = sc.nextDouble();
-            return firstValue + secondValue;
+        public static void sum(double firstValue, double secondValue){
+            double sum = firstValue + secondValue;
+            System.out.println("Summation = "+sum);
         }
+
+    public static void sub(double firstValue, double secondValue){
+        double sub = firstValue - secondValue;
+        System.out.println("Subtraction = "+sub);
     }
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double firstValue = sc.nextDouble();
+        double secondValue = sc.nextDouble();
+            Calculator calculator = new Calculator();
+            calculator.sum(firstValue,secondValue);
+            calculator.sub(firstValue,secondValue);
+    }
+    private double multiplication(double a, double b){
+            return a * b;
+    }
+}
